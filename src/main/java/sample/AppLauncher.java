7631472856cfc9e2dbd454a1030014c70b700da1
Cornea.Attila <attila.cornea@gmail.com>
2019-05-03 19:06:41 +0200

    Simple JavaFX functionality added.

---
+++ src/main/java/sample/AppLauncher.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class AppLauncher {
+
+    public static void main(String[] args)
+    {
+        FXApp.main(args);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module EncDecMVC
+{
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+
+    opens sample;
+}
