1caf93da6730d58bbf9d37b0258ee6f5498ff69f
Cornea.Attila <attila.cornea@gmail.com>
2019-05-04 13:13:51 +0200

    Model added.

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -2,5 +2,16 @@ package model;
 
 public class Model {
 
+    private void encrypt(String encPath, String encPass) {
 
+    }
+
+    private void decrypt(String decPath, String encPath) {
+
+    }
+
+    private String generate() {
+
+        return "";
+    }
 }
