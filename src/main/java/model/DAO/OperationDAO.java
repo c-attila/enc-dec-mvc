81f98c95ddbf9625d42f8fe9b5a268ac45e056e8
Cornea.Attila <attila.cornea@gmail.com>
2019-05-06 01:44:34 +0200

    Reading from database added.

--- src/main/java/model/DAO/OperationDAO.java
+++ src/main/java/model/DAO/OperationDAO.java
@@ -3,6 +3,8 @@ package model.DAO;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class OperationDAO {
 
@@ -18,6 +20,18 @@ public class OperationDAO {
         em.getTransaction().begin();
         em.persist(operation);
         em.getTransaction().commit();
+    }
+
+    public List<Operation> readOperationHistory(String operationType) {
+        TypedQuery<Operation> query = em.createQuery("SELECT o FROM Operation o WHERE o.operationType'" + operationType + "'", Operation.class);
+
+        return query.getResultList();
+    }
+
+    public List<Operation> readFileHistory(String path) {
+        TypedQuery<Operation> query = em.createQuery("SELECT o FROM Operation o WHERE o.path'" + path + "'", Operation.class);
+
+        return query.getResultList();
     }
 
     public static void close() {
